b8c1c3cb5cf7d197accc904f5e976b8bf7fe9504
BoykoNeov <boikoneov@gmail.com>
2017-12-19 11:57:55 +0200

    :tulip: started work on 'Multidimensional arrays' lab

---
+++ MultidimensionalArrays/Lab/SumMatrixElements/SumMatrixElementsStartUp.cs
@@ -0,0 +1,36 @@
+﻿namespace SumMatrixElements
+{
+    using System;
+    using System.Linq;
+
+    public class SumMatrixElementsStartUp
+    {
+        public static void Main()
+        {
+            int[] inputSizes = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowsCount = inputSizes[0];
+            int colsCount = inputSizes[1];
+
+            int[][] matrix = new int[rowsCount][];
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                matrix[i] = Console.ReadLine().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            long totalSum = 0;
+
+            foreach (int[] row in matrix)
+            {
+                foreach (int number in row)
+                {
+                    totalSum += number;
+                }
+            }
+
+            Console.WriteLine(rowsCount);
+            Console.WriteLine(colsCount);
+            Console.WriteLine(totalSum);
+        }
+    }
+}
