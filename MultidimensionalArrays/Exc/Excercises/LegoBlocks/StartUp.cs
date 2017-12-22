d1b667b9ec93568d384824aca6a7e6e5add5eed4
BoykoNeov <boikoneov@gmail.com>
2017-12-22 20:43:29 +0200

    :izakaya_lantern: Finished tasks 'Target practice' and 'Lego blocks' from 'Matrices'

---
+++ MultidimensionalArrays/Exc/Excercises/LegoBlocks/StartUp.cs
@@ -0,0 +1,59 @@
+﻿namespace LegoBlocks
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[][] firstArray = new string[n][];
+
+            // Read the first array
+            for (int i = 0; i < n; i++)
+            {
+                firstArray[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            // Read the second array, reverse its rows and add it to the first
+            for (int i = 0; i < n; i++)
+            {
+                string[] secondArrayRow = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string[] newRow = new string[secondArrayRow.Length + firstArray[i].Length];
+                Array.Reverse(secondArrayRow);
+                firstArray[i].CopyTo(newRow, 0);
+                secondArrayRow.CopyTo(newRow, firstArray[i].Length);
+                firstArray[i] = newRow;
+            }
+
+            // Check for symmetry
+            bool symmetrical = true;
+            int firstRowLength = firstArray[0].Length;
+            int totalNumberOfCells = firstRowLength;
+
+            for (int i = 1; i < firstArray.Length; i++)
+            {
+                totalNumberOfCells += firstArray[i].Length;
+
+                if (firstArray[i].Length != firstRowLength)
+                {
+                    symmetrical = false;
+                }
+            }
+
+            if (symmetrical)
+            {
+                for (int i = 0; i < firstArray.Length; i++)
+                {
+                    Console.Write("[");
+                    Console.WriteLine(string.Join(", ", firstArray[i]) + "]");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The total number of cells is: {totalNumberOfCells}");
+            }
+        }
+    }
+}
---
+++ MultidimensionalArrays/Exc/Excercises/TargetPractice/StartUp.cs
@@ -0,0 +1,94 @@
+﻿namespace TargetPractice
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            // Read input data
+            int[] matrixParams = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowsCount = matrixParams[0];
+            int columnsCount = matrixParams[1];
+
+            char[] snake = Console.ReadLine().ToCharArray();
+
+            int[] shotParams = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int impactRow = shotParams[0];
+            int impactColumn = shotParams[1];
+            int radius = shotParams[2];
+
+            // Generate matrix
+            char[][] matrix = new char[rowsCount][];
+
+            // Fill it with 'snake'
+            int snakeIndex = 0;
+            for (int i = rowsCount - 1; i >= 0; i--)
+            {
+                int currentRowEvenless = (rowsCount - 1 - i) % 2;
+                char[] currentRow = new char[columnsCount];
+
+                if (currentRowEvenless == 0)
+                {
+                    for (int j = currentRow.Length - 1; j >= 0; j--)
+                    {
+                        currentRow[j] = snake[snakeIndex++];
+                        snakeIndex = snakeIndex % snake.Length;
+                    }
+                }
+                else
+                {
+                    for (int j = 0; j < currentRow.Length; j++)
+                    {
+                        currentRow[j] = snake[snakeIndex++];
+                        snakeIndex = snakeIndex % snake.Length;
+                    }
+                }
+
+                matrix[i] = currentRow;
+            }
+
+            // Destroy snake elements
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    double calculatedDistanceFromShot = Math.Sqrt(Math.Pow(i - impactRow, 2) + Math.Pow(j - impactColumn, 2));
+
+                    if (calculatedDistanceFromShot <= radius)
+                    {
+                        matrix[i][j] = ' ';
+                    }
+                }
+            }
+
+            // Make elements fall
+            for (int i = matrix.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == ' ')
+                    {
+                        int upsearchIndex = i;
+
+                        while (upsearchIndex > 0 && matrix[upsearchIndex][j] == ' ')
+                        {
+                            upsearchIndex--;
+                        }
+
+                        char fallingDownValue = matrix[upsearchIndex][j];
+                        matrix[upsearchIndex][j] = ' ';
+                        matrix[i][j] = fallingDownValue;
+                    }
+                }
+            }
+
+            // Print the matrix
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                Console.WriteLine(string.Join("", matrix[i]));
+            }
+        }
+    }
+}
