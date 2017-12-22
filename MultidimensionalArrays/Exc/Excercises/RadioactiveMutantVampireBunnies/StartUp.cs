e056870417a1e7d06d5a7e7c1528deea0e8aafdc
BoykoNeov <boikoneov@gmail.com>
2017-12-23 00:20:08 +0200

    :rabbit2: Ongoing work on matrices problem - began 'radioactive mutant bunnies...'

---
+++ MultidimensionalArrays/Exc/Excercises/RadioactiveMutantVampireBunnies/StartUp.cs
@@ -0,0 +1,25 @@
+﻿namespace RadioactiveMutantVampireBunnies
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+       public static void Main()
+        {
+            int[] input = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowsCount = input[0];
+            int colsCount = input[1];
+
+            string[][] lair = new string[rowsCount][];
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                lair[i] = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            string playerMoves = Console.ReadLine();
+
+        }
+    }
+}
