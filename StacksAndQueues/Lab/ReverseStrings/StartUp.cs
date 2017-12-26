406e5a4cda096383fcc2bdd215e8feb861705a9b
BoykoNeov <boikoneov@gmail.com>
2017-12-27 00:19:34 +0200

    :tada: Added the first task from 'Stacks and queues' excercises

---
+++ StacksAndQueues/Lab/ReverseStrings/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace ReverseStrings
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Stack<char> stack = new Stack<char>(Console.ReadLine().ToCharArray());
+
+            StringBuilder sb = new StringBuilder();
+
+            while (stack.Count > 0)
+            {
+                sb.Append(stack.Pop());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
