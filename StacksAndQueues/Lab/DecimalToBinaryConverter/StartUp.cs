1747083900bd5972f18828112fe0bfc2420a768f
BoykoNeov <boikoneov@gmail.com>
2017-12-26 20:07:15 +0200

    :confetti_ball: adding some tasks to 'Stacks and Queues' lab

---
+++ StacksAndQueues/Lab/DecimalToBinaryConverter/StartUp.cs
@@ -0,0 +1,23 @@
+﻿namespace DecimalToBinaryConverter
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            long inputNumber = long.Parse(Console.ReadLine());
+            Stack<sbyte> remainders = new Stack<sbyte>();
+
+            do
+            {
+                remainders.Push((sbyte)(inputNumber % 2));
+                inputNumber = inputNumber / 2;
+            }
+            while (inputNumber != 0);
+
+            Console.WriteLine(string.Join("", remainders));
+        }
+    }
+}
---
+++ StacksAndQueues/Lab/SimpleCalculator/StartUp.cs
@@ -0,0 +1,37 @@
+﻿namespace SimpleCalculator
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Stack<string> input = new Stack<string>(Console.ReadLine().Split().Reverse());
+            decimal result = 0;
+
+            sbyte multiplier = 1;
+            while (input.Count > 0)
+            {
+                if (input.Peek() == "+")
+                {
+                    multiplier = 1;
+                    input.Pop();
+                }
+                else if(input.Peek() == "-")
+                {
+                    multiplier = -1;
+                    input.Pop();
+                }
+                else if (decimal.TryParse(input.Pop(), out decimal number))
+                {
+                    result += number * multiplier;
+                }
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
