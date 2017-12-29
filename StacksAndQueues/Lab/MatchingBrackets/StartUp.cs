2a426b78a75e263b9abf375280591996606e8871
BoykoNeov <boikoneov@gmail.com>
2017-12-29 09:10:15 +0200

    :tv: Finished 'Matching brackets' exc from 'Stacks and queues lab'

---
+++ StacksAndQueues/Lab/MatchingBrackets/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace MatchingBrackets
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            Stack<int> openingBracketsIndexes = new Stack<int>();
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    openingBracketsIndexes.Push(i);
+                }
+
+                if (input[i] == ')')
+                {
+                    int startIndex = openingBracketsIndexes.Pop();
+                    sb.AppendLine(input.Substring(startIndex, (i - startIndex + 1)));
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
