114898fd85565f6311b202917c16b9229e1d5538
BoykoNeov <boikoneov@gmail.com>
2017-12-30 11:40:53 +0200

    :twisted_rightwards_arrows: Finished task 'Hot Potato'

---
+++ StacksAndQueues/Lab/HotPotato/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace HotPotato
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string[] childNames = Console.ReadLine().Split(new char[] {' '} , StringSplitOptions.RemoveEmptyEntries);
+            Queue<string> game = new Queue<string>(childNames);
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while (game.Count > 1)
+            {
+                string currentChild = game.Dequeue();
+
+                if (counter == n)
+                {
+                    Console.WriteLine($"Removed {currentChild}");
+                    counter = 1;
+                }
+                else
+                {
+                    game.Enqueue(currentChild);
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Last is {game.Dequeue()}");
+        }
+    }
+}
