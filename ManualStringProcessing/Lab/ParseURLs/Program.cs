47c13bfa158c444bf130008de7b8adf59dd93ec1
BoykoNeov <boikoneov@gmail.com>
2018-01-03 15:15:40 +0200

    :white_square_button: Finished task 'parse URLs'

---
+++ ManualStringProcessing/Lab/ParseURLs/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ParseURLs
+{
+    using System;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            string[] splitByProtocol = input.Split(new string[] { "://" }, StringSplitOptions.None);
+
+            if (splitByProtocol.Length != 2)
+            {
+                Console.WriteLine("Invalid URL");
+            }
+            else
+            {
+                string[] splitByServer = splitByProtocol[1].Split(new char[] { '/' }, StringSplitOptions.None);
+
+                if (splitByServer.Length < 2)
+                {
+                    Console.WriteLine("Invalid URL");
+                }
+                else
+                {
+                    Console.WriteLine($"Protocol = {splitByProtocol[0]}");
+                    Console.WriteLine($"Server = {splitByServer[0]}");
+                    Console.WriteLine($"Resources = {string.Join("/", splitByServer.Skip(1))}");
+                }
+            }
+        }
+    }
+}
