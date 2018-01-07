82d52d407e83f3ec71681df54e1187d3475f74a6
BoykoNeov <boikoneov@gmail.com>
2018-01-07 20:04:32 +0200

    :ophiuchus: Finished task 'Parse Tags'

---
+++ ManualStringProcessing/Lab/ParseTags/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace ParseTags
+{
+    using System;
+    using System.Text.RegularExpressions;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            const string Pattern = @"(<upcase>)(.*?)(<\/upcase>)";
+            Regex regex = new Regex(Pattern);
+            string input = Console.ReadLine();
+            
+            string replaced = Regex.Replace(input, Pattern, m => m.Groups[2].Value.ToUpper());
+            Console.WriteLine(replaced);
+        }
+    }
+}
