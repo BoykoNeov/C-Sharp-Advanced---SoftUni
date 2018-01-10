bc726196c9279bd0819494b902d76bf4fa4443ab
BoykoNeov <boikoneov@gmail.com>
2018-01-10 12:35:23 +0200

    :non-potable_water: Finished task 'Special words'

---
+++ ManualStringProcessing/Lab/SpecialWords/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SpecialWords
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            char[] separators = new char[] { '(', ')', '[', ']', '<', '>', ',', '-', '!', '?', ' ' };
+            string[] words = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            string[] textWords = Console.ReadLine().Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                wordCounts[word] = 0;
+            }
+
+            foreach (string textWord in textWords)
+            {
+                if (wordCounts.ContainsKey(textWord))
+                {
+                    wordCounts[textWord]++;
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (KeyValuePair<string,int> wordCount in wordCounts)
+            {
+                sb.AppendLine(wordCount.Key + " - " + wordCount.Value);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
