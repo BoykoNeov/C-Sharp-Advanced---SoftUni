f888ea228b7e29986eae7afa98c2bd91e6939aab
BoykoNeov <boikoneov@gmail.com>
2018-01-12 21:25:53 +0200

    :diamond_shape_with_a_dot_inside: Finished task 'concatenate strings' , removed one unnessesary file

---
+++ ManualStringProcessing/Lab/ConcatenateStrings/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace ConcatenateStrings
+{
+    using System;
+    using System.Text;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(Console.ReadLine() + " ");
+            }
+
+            Console.WriteLine(sb.ToString().Trim());
+        }
+    }
+}
