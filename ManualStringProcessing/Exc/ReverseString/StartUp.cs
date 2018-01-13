3bf3172b834986290bf26944041b152cb626caf6
BoykoNeov <boikoneov@gmail.com>
2018-01-13 23:27:01 +0200

    :taurus: Started excercises from 'Manual string processing'

---
+++ ManualStringProcessing/Exc/ReverseString/StartUp.cs
@@ -0,0 +1,16 @@
+﻿namespace ReverseString
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+            char[] inputarray = input.ToCharArray();
+            Array.Reverse(inputarray);
+            string output = new string(inputarray);
+            Console.WriteLine(output);
+        }
+    }
+}
