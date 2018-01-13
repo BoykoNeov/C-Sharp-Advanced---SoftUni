4dc91c5dd15946e974f43c22bd782de061f810a2
BoykoNeov <boikoneov@gmail.com>
2018-01-14 01:07:25 +0200

    :soon: finished second task from 'manual string processing'

---
+++ ManualStringProcessing/Exc/StringLength/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace StringLength
+{
+    using System;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            const int charCount = 20;
+
+            char[] input = Console.ReadLine().Take(charCount).ToArray();
+            char[] outputArray = new char[charCount];
+
+            for (int i = 0; i < charCount; i++)
+            {
+                if (i < input.Length)
+                {
+                    outputArray[i] = input[i];
+                }
+                else
+                {
+                    outputArray[i] = '*';
+                }
+            }
+
+            string output = new string(outputArray);
+            Console.WriteLine(output);
+        }
+    }
+}
