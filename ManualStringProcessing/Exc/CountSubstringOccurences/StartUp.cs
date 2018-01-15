991e263ee2253e56a870faa3e8c0102b8e13caaf
BoykoNeov <boikoneov@gmail.com>
2018-01-16 00:26:40 +0200

    :o: Completed 'Count substring occurunces'

---
+++ ManualStringProcessing/Exc/CountSubstringOccurences/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace CountSubstringOccurences
+{
+    using System;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string text = Console.ReadLine().ToLower();
+            string substring = Console.ReadLine().ToLower();
+            int substringCount = 0;
+            int currentSubstringIndex = 0;
+
+
+            do
+            {
+                currentSubstringIndex = text.IndexOf(substring, currentSubstringIndex);
+                currentSubstringIndex++;
+
+                if (currentSubstringIndex > 0)
+                {
+                    substringCount++;
+                }
+            }
+            while (currentSubstringIndex > 0 && currentSubstringIndex < text.Length);
+
+            Console.WriteLine(substringCount);
+        }
+    }
+}
