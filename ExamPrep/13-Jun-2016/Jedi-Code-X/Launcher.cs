e9567852871f73cf031c2cd963edfbde7a7d5d77
BoykoNeov <boikoneov@gmail.com>
2018-02-09 16:37:04 +0200

    :grapes: work on jedi code-x

---
+++ ExamPrep/13-Jun-2016/Jedi-Code-X/Launcher.cs
@@ -0,0 +1,90 @@
+﻿namespace Jedi_Code_X
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Text.RegularExpressions;
+
+    public class Launcher
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] lines = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                lines[i] = Console.ReadLine();
+            }
+
+            string jediInputPattern = Console.ReadLine();
+            string messageInputPattern = Console.ReadLine();
+
+            int[] jediMessagesIndex = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x) - 1).ToArray();
+
+
+
+            //string pattern = string.Format("(?<message>{0}[a-zA-Z0-9]{{{1}}})[^a-zA-Z0-9]?|(?<jedi>{2}[a-zA-Z]{{{3}}})[^a-zA-Z]?", messageInputPattern, messageInputPattern.Length, jediInputPattern, jediInputPattern.Length);
+
+            string pattern = string.Format("(?<message>{0}[a-zA-Z0-9]{{{1}}})(?![a-zA-Z0-9])|(?<jedi>{2}[a-zA-Z]{{{3}}})(?![a-zA-Z])", messageInputPattern, messageInputPattern.Length, jediInputPattern, jediInputPattern.Length);
+
+            List<string> jedis = new List<string>();
+            List<string> messages = new List<string>();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                MatchCollection matches = Regex.Matches(lines[i], pattern);
+
+                foreach (Match match in matches)
+                {
+                    if (match.Groups["message"].Value.Length > 0)
+                    {
+                        messages.Add(match.Groups["message"].Value.Substring(messageInputPattern.Length));
+                    }
+
+                    if (match.Groups["jedi"].Value.Length > 0)
+                    {
+                        jedis.Add(match.Groups["jedi"].Value.Substring(jediInputPattern.Length));
+                    }
+                }
+            }
+
+            Dictionary<string, string> jediMessages = new Dictionary<string, string>();
+
+            int jediIndex = 0;
+
+            for (int i = 0; i < jediMessagesIndex.Length; i++)
+            {
+                if (jediMessagesIndex[i] < 0 || jediMessagesIndex[i] >= messages.Count)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (jediIndex < jedis.Count)
+                    {
+                        jediMessages[jedis[jediIndex]] = messages[jediMessagesIndex[i]];
+                        jediIndex++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+
+                }
+            }
+
+            foreach (string jedi in jedis)
+            {
+                if (jediMessages.ContainsKey(jedi))
+                {
+                    Console.WriteLine($"{jedi} - {jediMessages[jedi]}");
+                }
+            }
+        }
+    }
+}
--- ExamPrep/3-Sep-2017/TreasureMap/Launcher.cs
+++ ExamPrep/3-Sep-2017/TreasureMap/Launcher.cs
@@ -9,20 +9,17 @@
         {
             int n = int.Parse(Console.ReadLine());
 
-            // this does not work
+            // this does not work because there may be more digits after the password, somewhere in the capture and it is still valid
             //   string pattern = @"(!|#)[^!#]*?\b(?<street>[A-Za-z]{4})\b[^!#]*[^\d](?<number>\d{3})-(?<password>\d{6}|\d{4})(?:[^\d!#]*)?(?!\1)(#|!)";
 
-            string pattern =      @"(!|#)[^!#]*?\b(?<street>[A-Za-z]{4})\b[^!#]*[^\d](?<number>\d{3})-(?<password>\d{6}|\d{4})(?:[^\d!#][^!#]*)?(?!\1)(#|!)";
+            string pattern = @"(!|#)[^!#]*?\b(?<street>[A-Za-z]{4})\b[^!#]*(?<!\d)(?<number>\d{3})-(?<password>\d{6}|\d{4})(?!\d)[^!#]*?(?!\1)(#|!)";
 
             for (int i = 0; i < n; i++)
             {
                 MatchCollection matches = Regex.Matches(Console.ReadLine(), pattern);
 
-                if (matches.Count > 0)
-                {
-                    Match a = matches[matches.Count / 2];
-                    Console.WriteLine($"Go to str. {a.Groups["street"].Value.ToString()} {a.Groups["number"].Value.ToString()}. Secret pass: {a.Groups["password"].Value.ToString()}.");
-                }
+                Match a = matches[matches.Count / 2];
+                Console.WriteLine($"Go to str. {a.Groups["street"].Value.ToString()} {a.Groups["number"].Value.ToString()}. Secret pass: {a.Groups["password"].Value.ToString()}.");
             }
         }
     }
