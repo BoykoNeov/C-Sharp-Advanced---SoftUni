dd8cf436ae841792c25a280ebe028c636c00f719
BoykoNeov <boikoneov@gmail.com>
2018-02-09 17:08:22 +0200

    :tomato: jedi-x-code an important step was to combine all lines into single text

--- ExamPrep/13-Jun-2016/Jedi-Code-X/Launcher.cs
+++ ExamPrep/13-Jun-2016/Jedi-Code-X/Launcher.cs
@@ -4,52 +4,44 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using System.Threading.Tasks;
     using System.Text.RegularExpressions;
 
     public class Launcher
     {
         public static void Main()
         {
+            StringBuilder sb = new StringBuilder();
+
             int n = int.Parse(Console.ReadLine());
-
-            string[] lines = new string[n];
-
             for (int i = 0; i < n; i++)
             {
-                lines[i] = Console.ReadLine();
+                sb.Append(Console.ReadLine());
             }
 
             string jediInputPattern = Console.ReadLine();
             string messageInputPattern = Console.ReadLine();
 
-            int[] jediMessagesIndex = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x) - 1).ToArray();
+            int[] jediMessagesIndex = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x) - 1).ToArray();
 
-
-
-            //string pattern = string.Format("(?<message>{0}[a-zA-Z0-9]{{{1}}})[^a-zA-Z0-9]?|(?<jedi>{2}[a-zA-Z]{{{3}}})[^a-zA-Z]?", messageInputPattern, messageInputPattern.Length, jediInputPattern, jediInputPattern.Length);
-
-            string pattern = string.Format("(?<message>{0}[a-zA-Z0-9]{{{1}}})(?![a-zA-Z0-9])|(?<jedi>{2}[a-zA-Z]{{{3}}})(?![a-zA-Z])", messageInputPattern, messageInputPattern.Length, jediInputPattern, jediInputPattern.Length);
+            string pattern = string.Format("(?<jedi>{0}[a-zA-Z]{{{1}}})(?![a-zA-Z])|(?<message>{2}[a-zA-Z0-9]{{{3}}})(?![a-zA-Z0-9])", jediInputPattern, jediInputPattern.Length, messageInputPattern, messageInputPattern.Length);
 
             List<string> jedis = new List<string>();
             List<string> messages = new List<string>();
 
+            string messageToDecode = sb.ToString();
 
-            for (int i = 0; i < n; i++)
+            MatchCollection matches = Regex.Matches(messageToDecode, pattern);
+
+            foreach (Match match in matches)
             {
-                MatchCollection matches = Regex.Matches(lines[i], pattern);
-
-                foreach (Match match in matches)
+                if (match.Groups["message"].Value.Length > 0)
                 {
-                    if (match.Groups["message"].Value.Length > 0)
-                    {
-                        messages.Add(match.Groups["message"].Value.Substring(messageInputPattern.Length));
-                    }
+                    messages.Add(match.Groups["message"].Value.Substring(messageInputPattern.Length));
+                }
 
-                    if (match.Groups["jedi"].Value.Length > 0)
-                    {
-                        jedis.Add(match.Groups["jedi"].Value.Substring(jediInputPattern.Length));
-                    }
+                if (match.Groups["jedi"].Value.Length > 0)
+                {
+                    jedis.Add(match.Groups["jedi"].Value.Substring(jediInputPattern.Length));
                 }
             }
 
@@ -74,7 +66,6 @@
                     {
                         break;
                     }
-
                 }
             }
 
